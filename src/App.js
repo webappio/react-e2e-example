16686baaf5b17fa0a23a1b615e320b2c45b0deae
Colin Chartier <colin@webapp.io>
2022-02-03 11:27:27 -0400

    Add page contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import cypress from './static/cypress.svg';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import './App.css';
 
+function Header() {
+    return <header className="main-header">
+        <Link to="/page1">Some page</Link>
+        <Link to="/page2">Another page</Link>
+        <Link to="/page3">A third page</Link>
+    </header>
+}
+
+function Home() {
+    return <main className="App">
+        <Header />
+        <div className="page-content">
+            <h1 className="main-tagline">
+                <img src={cypress} alt="" className="cypress-logo"/> Cypress example for React
+            </h1>
+        </div>
+    </main>
+}
+
+function PageView({name}) {
+    return <main className="App">
+        <Header />
+        <div className="page-content">
+            <h1 className="main-tagline">
+                {name}
+            </h1>
+        </div>
+    </main>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Router>
+        <Routes>
+            <Route path="/page1" exact element={<PageView name="Page one"/>} />
+            <Route path="/page2" exact element={<PageView name="Page two"/>} />
+            <Route path="/page3" exact element={<PageView name="Page three"/>} />
+            <Route exact path="/" element={<Home />} />
+        </Routes>
+    </Router>
 }
 
 export default App;
